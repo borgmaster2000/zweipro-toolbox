4c9630c96f95573c05c1ab71be0f85e2e355cc2e
borgmaster2000 <admin@zweipro.de>
2026-01-14 14:42:29 +0100

    Bump version to 1.4.7

--- zweipro-toolbox.php
+++ zweipro-toolbox.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ZWEIPRO Toolbox
  * Description: Sammlung von Utility-Modulen (SMTP, Snippets, Captcha, Cookie Banner, etc.).
- * Version: 1.4.5
+ * Version: 1.4.7
  * Author: ZWEIPRO
  * Text Domain: zweipro-toolbox
  */
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('ZWEIPRO_TOOLBOX_VERSION', '1.4.5');
+define('ZWEIPRO_TOOLBOX_VERSION', '1.4.7');
 define('ZWEIPRO_TOOLBOX_PATH', plugin_dir_path(__FILE__));
 define('ZWEIPRO_TOOLBOX_URL', plugin_dir_url(__FILE__));
 
